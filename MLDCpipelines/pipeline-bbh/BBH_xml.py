2fdeac34aa5ad68e7459f052779e3bc081f503d3
Stas Babak <stasbabak@googlemail.com>
2006-07-27 17:51:50 +0000

    Buge related to the duration of the BBH signal was resolved. Requires updated version of synthetic LISA
    git-svn-id: https://lisatools.googlecode.com/svn@59 c0d0b04f-df38-0410-a060-df9e05943d0e

--- MLDCpipelines/pipeline-bbh/BBH_xml.py
+++ MLDCpipelines/pipeline-bbh/BBH_xml.py
@@ -76,20 +76,21 @@ if ( not outXMLfile):
 
 random.seed(seed)
 taper = 8.0
-dt = 15.0 # time step seconds
+dt = 15.0               # time step seconds
 truncateTime = 0.0
-duration = 31457280.0
-
+timeoffset = -900       # Defines t0
+#duration = 31457280.0
+duration = (2**21)*15 - timeoffset    # tEnd, actual duration is tEnd-t0
 
 m1   = 1.e6*random.uniform(1.0, 5.0) 
 m2   = m1/random.uniform(1.0,4.0)    
-cS   = random.uniform(-1.0, 1.0)           # cos(thetaS)
-phS  = random.uniform(0.0, math.pi*2.0);   # phiS
-ci   = random.uniform(-1.0, 1.0);          # cos(inclination)
-psi  = random.uniform(0.0, math.pi*2.0);   # polarization
-Phi0 = random.uniform(0.0, math.pi*2.0);   # initial orbital phase
-u    = random.uniform(-20., 20.);
-tc   = (Tc + u)*24.*3600.;                 # coalescence time in sec
+cS   = random.uniform(-1.0, 1.0)          # cos(thetaS)
+phS  = random.uniform(0.0, math.pi*2.0)   # phiS
+ci   = random.uniform(-1.0, 1.0)          # cos(inclination)
+psi  = random.uniform(0.0, math.pi*2.0)   # polarization
+Phi0 = random.uniform(0.0, math.pi*2.0)   # initial orbital phase
+u    = random.uniform(-20., 20.)
+tc   = (Tc + u)*24.*3600.                 # coalescence time in sec
 
 #print "otput file = ", outXMLfile
 #print "TC =  %f (days)" % Tc
@@ -98,16 +99,15 @@ tc   = (Tc + u)*24.*3600.;                 # coalescence time in sec
 
 
 # estimate initial orbital angular freq
-omega0 = lisawp.PNBinaryInitialFrequency(m1, m2, tc) # initial orb. ang. freq.
-print "Initial GW freq = ", omega0/math.pi
-f0 = omega0/math.pi    # initial freq of GW signal
+#omega0 = lisawp.PNBinaryInitialFrequency(m1, m2, tc) # initial orb. ang. freq.
+#print "Initial GW freq = ", omega0/math.pi
+#f0 = omega0/math.pi    # initial freq of GW signal
 
-timeoffset = -900
 theta = math.acos(ci)
 elat = math.acos(cS)
 elon = phS
 pol = psi
-mysystem = lisawp.PNBinary(m1,m2,omega0,Phi0,D,theta,elat,elon,pol,
+mysystem = lisawp.PNBinary(m1,m2,tc,Phi0,D,theta,elat,elon,pol,
                            timeoffset,dt,truncateTime,taper,duration)
 
 outputXML = lisaXML(outXMLfile,author='Stas Babak')
