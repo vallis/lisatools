7cf1b9a6bfedf69088af9fc581023a8c65d4937b
Michele Vallisneri <vallis@vallis.org>
2006-10-10 20:58:47 +0000

    Initial try
    git-svn-id: https://lisatools.googlecode.com/svn@88 c0d0b04f-df38-0410-a060-df9e05943d0e

---
+++ lisaXML/io-python/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+
+setup(name = 'BBH',
+      version = '$Id $',
+#     description = '',
+      author = 'Stas Babak',
+      author_email = 'stba@aei.mpg.de>',
+#     url = '',
+      
+      py_modules = ['BBH'],
+      
+      ext_modules = [Extension('_BBH',
+                               ['BBHChallenge1.cc', 'Constants.cc', 'BBH.i'],
+                               depends = ['BBHChallenge1.hh', 'Constants.hh', 'Macros.hh'])]
+      )
+
+# I'm specifying that we want SWIG to create a CPP extension in setup.cfg...
+# how do I set which 
