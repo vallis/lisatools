c3b37d20e701bc6946cfb63df15b2ba78f67bfd0
Michele Vallisneri <vallis@vallis.org>
2006-11-26 04:35:21 +0000

    Problem...
    git-svn-id: https://lisatools.googlecode.com/svn@146 c0d0b04f-df38-0410-a060-df9e05943d0e

--- MLDCwaveforms/MLDC/__init__.py
+++ MLDCwaveforms/MLDC/__init__.py
@@ -2,6 +2,8 @@ from sys import stderr
 from glob import glob
 from re import sub
 
+# does not seem to work correctly right now...
+
 for p in glob(__path__[0] + '/*.py'):
     if not '__init__.py' in p:
         modulename = sub('.*/(.*)\.py','MLDC.\\1',p)
