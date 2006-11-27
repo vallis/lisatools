f4cfeebb8cf434cfb65bd5e57d44a01e6858dc4f
Michele Vallisneri <vallis@vallis.org>
2006-11-27 05:19:57 +0000

    Handle test and true galaxy
    git-svn-id: https://lisatools.googlecode.com/svn@155 c0d0b04f-df38-0410-a060-df9e05943d0e

--- MLDCpipelines2/bin/makeTDIsignals-Galaxy.py
+++ MLDCpipelines2/bin/makeTDIsignals-Galaxy.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 
 import os
+import sys
+
+import lisaxml
+
+def run(command):
+    commandline = command % globals()
+    print "---> %s" % commandline
+    
+    try:
+        assert(os.system(commandline) == 0)
+    except:
+        print 'Script %s failed at command "%s".' % (sys.argv[0],commandline)
+        sys.exit(1)
 
 # only one argument, and it's the seed...
 seed = int(sys.argv[1])
@@ -9,18 +22,28 @@ here = os.getcwd()
 os.chdir('../MLDCwaveforms/Galaxy')
 
 # compute the instrument response (takes a couple of hours)
-assert(0 == os.system('./Fast_Response 1')) 
-assert(0 == os.system('./Fast_XML_LS Test Data/Sources_LS.txt')) 
-assert(0 == os.system('./Fast_XML_SL Test Data/Sources_SL.txt')) 
+run('./Fast_Response %s' % seed)
+run('./Fast_XML_LS TheGalaxy Data/Sources_LS.txt') 
+run('./Fast_XML_SL TheGalaxy Data/Sources_SL.txt')
+
+# if lisaxml knew how to handle tables, this would copy the key information into the TDI file...
+# it will do so very soon...
+# but since the table dat files are so big, maybe they should just be copied or moved, not
+# parsed and rewritten...
+
+# the following, however, handles copying the binaries
+
+sltdiobj = lisaxml.lisaXML(here + '/TDI/Galaxy-tdi-frequency.xml')
+sltdiobj.close()
+
+run('%s/bin/mergeXML.py %s/TDI/Galaxy-tdi-frequency.xml XML/TheGalaxy_synthLISA_noisefree.xml %s/Galaxy/Galaxy.xml' % (here,here,here))
+
+lstdiobj = lisaxml.lisaXML(here + '/TDI/Galaxy-tdi-strain.xml')
+lstdiobj.close()
+
+run('%s/bin/mergeXML.py %s/TDI/Galaxy-tdi-strain.xml XML/TheGalaxy_LISAsim_noisefree.xml %s/Galaxy/Galaxy.xml' % (here,here,here))
+
+run('rm Binary/TheGalaxy_*.bin')
+# there should be more cleaning to do...
 
 os.chdir(here)
-
-assert(0 == os.system('cp ../MLDCwaveforms/Galaxy/XML/Test_synthLISA_noisefree.xml TDI/Test-tdi-frequency.xml'))
-assert(0 == os.system('cp ../MLDCwaveforms/Galaxy/XML/Test_LISAsim_noisefree.xml   TDI/Test-tdi-frequency.xml'))
-
-assert(0 == os.system('mkdir TDI/Binary'))
-assert(0 == os.system('cp ../MLDCwaveforms/Galaxy/Binary/Test_synthLISA_noisefree.bin TDI/Binary/.'))
-assert(0 == os.system('cp ../MLDCwaveforms/Galaxy/Binary/Test_LISAsim_noisefree.bin   TDI/Binary/.'))
-
-# this should relocate the binaries...
-assert(0 == os.system('bin/mergeXML.py TDI/Test-tdi-frequency.xml Source/TestGalaxy.xml'))
