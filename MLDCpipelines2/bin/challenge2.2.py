c08d9c8d52fae14eb03cf0879b809cb78749b1b7
Stas Babak <stasbabak@googlemail.com>
2006-12-16 17:10:46 +0000

    Distance (default) for BBH was changed
    git-svn-id: https://lisatools.googlecode.com/svn@245 c0d0b04f-df38-0410-a060-df9e05943d0e

--- MLDCpipelines2/bin/challenge2.2.py
+++ MLDCpipelines2/bin/challenge2.2.py
@@ -19,10 +19,10 @@ seed = int(sys.argv[1])
 # challenge 2.2 BBH sources...
 
 # 1 - A loud (SNR ~ 2000) signal (with in-spiral "in band", that is from f = 0.1 mHz, lasting about 1 week) and coalescing 3 months +/- 1 month after the beginning of the observations
-run('bin/makesource-BBH.py --seed=%s --distance=1e9 --requestSN=2000 --coalescTime=90  --coalescRange=30 Source/BH-1.xml' % (seed+1))
+run('bin/makesource-BBH.py --seed=%s --distance=1e10 --requestSN=2000 --coalescTime=90  --coalescRange=30 Source/BH-1.xml' % (seed+1))
 
 # 2 - A moderate-to-low SNR (~ 20) signal with coalescence between 25 and 26 months from the beginning of the observations
-run('bin/makesource-BBH.py --seed=%s --distance=1e9 --requestSN=20   --coalescTime=765 --coalescRange=15 Source/BH-2.xml' % (seed+2))
+run('bin/makesource-BBH.py --seed=%s --distance=1e10 --requestSN=20   --coalescTime=765 --coalescRange=15 Source/BH-2.xml' % (seed+2))
 
 # the following BBHs may or may not be included in the challenge, depending on fate...
 
@@ -32,19 +32,19 @@ random.seed(seed)
 
 # 3 - A loud (SNR ~ 1000) signal coalescing between 6 and 24 months from beginning of observations
 if random.random() > 0.5:
-    run('bin/makesource-BBH.py --seed=%s --distance=1e9 --requestSN=1000 --coalescTime=450 --coalescRange=270 Source/BH-3.xml' % (seed+3))
+    run('bin/makesource-BBH.py --seed=%s --distance=1e10 --requestSN=1000 --coalescTime=450 --coalescRange=270 Source/BH-3.xml' % (seed+3))
 
 # 4 - A loud (SNR ~ 200) signal coalescing between 6 and 24 months from beginning of observations
 if random.random() > 0.5:
-    run('bin/makesource-BBH.py --seed=%s --distance=1e9 --requestSN=200  --coalescTime=450 --coalescRange=270 Source/BH-4.xml' % (seed+4))
+    run('bin/makesource-BBH.py --seed=%s --distance=1e10 --requestSN=200 --coalescTime=450 --coalescRange=270 Source/BH-4.xml' % (seed+4))
 
 # 5 - A loud (SNR ~ 100) signal with coalescence taking place between 18 and 21 months from the start of the observations
 if random.random() > 0.5:
-    run('bin/makesource-BBH.py --seed=%s --distance=1e9 --requestSN=100  --coalescTime=540 --coalescRange=45  Source/BH-5.xml' % (seed+5))
+    run('bin/makesource-BBH.py --seed=%s --distance=1e10 --requestSN=100 --coalescTime=540 --coalescRange=45 Source/BH-5.xml' % (seed+5))
 
 # 6 - A low SNR (~ 10) signal with coalescence taking place between 27 and 28 months from the beginning of the observations
 if random.random() > 0.5:
-    run('bin/makesource-BBH.py --seed=%s --distance=1e9 --requestSN=10   --coalescTime=825 --coalescRange=15  Source/BH-6.xml' % (seed+6))
+    run('bin/makesource-BBH.py --seed=%s --distance=1e10 --requestSN=10 --coalescTime=825 --coalescRange=15 Source/BH-6.xml' % (seed+6))
 
 # challenge 2.2 EMRI sources...
 # 2 with SMBH around 1e6, 2 with SMBH around 5e6, 1 with SMBH around 1e7
