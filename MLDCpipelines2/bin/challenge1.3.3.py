0fdeb1a1b371f62ab80ea616c47a0727d5ad1cfc
Stas Babak <stasbabak@googlemail.com>
2007-02-01 10:28:19 +0000

    Scripts for challenge 1.3 (EMRIs)
    git-svn-id: https://lisatools.googlecode.com/svn@324 c0d0b04f-df38-0410-a060-df9e05943d0e

---
+++ MLDCpipelines2/bin/challenge1.3.3.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import random
+
+
+def run(command):
+    commandline = command % globals()
+    print "----> %s" % commandline
+    
+    try:
+        assert(os.system(commandline) == 0)
+    except:
+        print 'Script %s failed at command "%s".' % (sys.argv[0],commandline)
+        sys.exit(1)
+
+# only one argument, and it's the seed...
+seed = int(sys.argv[1])
+
+#Challenge 1.3.3: One EMRI. MBH mass = 5.e6, snr random between 40 and 110
+
+requestsn = random.randint(40,110)
+run('bin/makesource-EMRI.py --seed=%s --distance=1e9 --requestSN=%s --massSMBH=5.e6 --sourceName="EMRI-1.3.3" Source/EMRI-1.3.3.xml' % (seed,requestsn))
+
+
+
