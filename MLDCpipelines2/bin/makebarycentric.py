9becf825241e4f1acd24facab16679f79bec68a7
Michele Vallisneri <vallis@vallis.org>
2007-05-22 02:05:15 +0000

    Allow for lisaXML files with multiple sources (but output to multiple barycentric files with similar names)
    git-svn-id: https://lisatools.googlecode.com/svn@397 c0d0b04f-df38-0410-a060-df9e05943d0e

--- MLDCpipelines2/bin/makebarycentric.py
+++ MLDCpipelines2/bin/makebarycentric.py
@@ -6,6 +6,7 @@ import lisaxml
 from lisaxml.convertunit import convertUnit
 
 import sys
+import re
 import math
 
 # set ourselves up to parse command-line options
@@ -59,55 +60,66 @@ if len(args) != 2:
 
 (inputfile,outputfile) = args
 
-# work only with the first source of the first file
+# cycle through all the sources in the file
 
 inputXML = lisaxml.readXML(inputfile)
-mysystem = inputXML.getLISASources()[0]
+
+allsystems = inputXML.getLISASources()
+
+for mysystem, cnt in zip(allsystems,range(len(allsystems))):
+    # print out parameters
+
+    if options.verbose:
+        for p in mysystem.parameters:
+            print p, ':', mysystem.parstr(p)
+
+    # we are going to override IntegrationStep, if defined by the source, with
+    # our own options.timestep. In the future it may be nice to provide for
+    # interpolation...
+
+    if ( hasattr(mysystem,'IntegrationStep') and
+        convertUnit(mysystem.IntegrationStep,mysystem.IntegrationStep_Unit,'Second') != (options.timestep,'Second') ):
+        print >> sys.stderr, "Overriding source IntegrationStep (%s) with our own timestep (%s s)" % (mysystem.parstr('IntegrationStep'),
+                                                                                                      options.timestep)
+
+    initialtime = options.inittime - options.prebuffer
+    samples = int( (options.duration + options.prebuffer + options.postbuffer) / options.timestep + 0.1 )
+
+    if options.poldebug == True and mysystem.xmltype == 'ExtremeMassRatioInspiral':
+        (hp0,hc0) = mysystem.waveforms(samples,options.timestep,initialtime,debug=-1)
+    else:
+        (hp0,hc0) = mysystem.waveforms(samples,options.timestep,initialtime)
+
+    if options.debug == True and mysystem.xmltype == 'ExtremeMassRatioInspiral':
+        (Ap,Ac) = mysystem.waveforms(samples,options.timestep,initialtime,debug=1)
+        (ga,al) = mysystem.waveforms(samples,options.timestep,initialtime,debug=2)
+        (ps,pl) = mysystem.waveforms(samples,options.timestep,initialtime,debug=3)
+
+    # impose polarization on waveform
+
+    pol = mysystem.Polarization
+
+    hp =  math.cos(2*pol) * hp0 + math.sin(2*pol) * hc0
+    hc = -math.sin(2*pol) * hp0 + math.cos(2*pol) * hc0
+
+    if options.debug == True and mysystem.xmltype == 'ExtremeMassRatioInspiral':
+        mysystem.TimeSeries = lisaxml.TimeSeries((hp,hc,Ap,Ac,ga,al,ps,pl),'hp,hc,Ap,Ac,gamma,alpha,psi,psisl')
+    else:
+        mysystem.TimeSeries = lisaxml.TimeSeries((hp,hc),'hp,hc')
+
+    mysystem.TimeSeries.Cadence = options.timestep
+    mysystem.TimeSeries.TimeOffset = initialtime
+
+    if len(allsystems) == 1:
+        oneoutputfile = outputfile
+    else:
+        if '.xml' in outputfile:
+            oneoutputfile = re.sub('\.xml','-%d.xml' % cnt,outputfile)
+        else:
+            oneoutputfile = outputfile + ('-%d' % cnt)
+
+    outputXML = lisaxml.lisaXML(oneoutputfile,author='Michele Vallisneri')
+    outputXML.SourceData(mysystem)
+    outputXML.close()
+
 inputXML.close()
-
-# print out parameters
-
-if options.verbose:
-    for p in mysystem.parameters:
-        print p, ':', mysystem.parstr(p)
-
-# we are going to override IntegrationStep, if defined by the source, with
-# our own options.timestep. In the future it may be nice to provide for
-# interpolation...
-
-if ( hasattr(mysystem,'IntegrationStep') and
-    convertUnit(mysystem.IntegrationStep,mysystem.IntegrationStep_Unit,'Second') != (options.timestep,'Second') ):
-    print >> sys.stderr, "Overriding source IntegrationStep (%s) with our own timestep (%s s)" % (mysystem.parstr('IntegrationStep'),
-                                                                                                  options.timestep)
-
-initialtime = options.inittime - options.prebuffer
-samples = int( (options.duration + options.prebuffer + options.postbuffer) / options.timestep + 0.1 )
-
-if options.poldebug == True and mysystem.xmltype == 'ExtremeMassRatioInspiral':
-    (hp0,hc0) = mysystem.waveforms(samples,options.timestep,initialtime,debug=-1)
-else:
-    (hp0,hc0) = mysystem.waveforms(samples,options.timestep,initialtime)
-
-if options.debug == True and mysystem.xmltype == 'ExtremeMassRatioInspiral':
-    (Ap,Ac) = mysystem.waveforms(samples,options.timestep,initialtime,debug=1)
-    (ga,al) = mysystem.waveforms(samples,options.timestep,initialtime,debug=2)
-    (ps,pl) = mysystem.waveforms(samples,options.timestep,initialtime,debug=3)
-
-# impose polarization on waveform
-
-pol = mysystem.Polarization
-
-hp =  math.cos(2*pol) * hp0 + math.sin(2*pol) * hc0
-hc = -math.sin(2*pol) * hp0 + math.cos(2*pol) * hc0
-
-if options.debug == True and mysystem.xmltype == 'ExtremeMassRatioInspiral':
-    mysystem.TimeSeries = lisaxml.TimeSeries((hp,hc,Ap,Ac,ga,al,ps,pl),'hp,hc,Ap,Ac,gamma,alpha,psi,psisl')
-else:
-    mysystem.TimeSeries = lisaxml.TimeSeries((hp,hc),'hp,hc')
-
-mysystem.TimeSeries.Cadence = options.timestep
-mysystem.TimeSeries.TimeOffset = initialtime
-
-outputXML = lisaxml.lisaXML(outputfile,author='Michele Vallisneri')
-outputXML.SourceData(mysystem)
-outputXML.close()
