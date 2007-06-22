903e64f39d6a7c9a724f7d82c74db41d9d06c7b6
Michele Vallisneri <vallis@vallis.org>
2007-06-22 18:33:16 +0000

    Stand-alone LISAsim-calling commands
    git-svn-id: https://lisatools.googlecode.com/svn@434 c0d0b04f-df38-0410-a060-df9e05943d0e

---
+++ MLDCpipelines2/bin/makeTDInoise-lisasim.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+
+__version__='$Id: $'
+
+import sys
+import glob
+import os
+import tempfile
+import re
+from distutils.dep_util import newer
+
+import lisaxml
+
+def run(command,quiet = False):
+    commandline = command % globals()
+    print "----> %s" % commandline
+    
+    try:
+        if not quiet:
+            assert(os.system(commandline) == 0)
+        else:
+            assert(os.system(commandline + ' > /dev/null') == 0)
+    except:
+        print 'Script %s failed at command "%s".' % (sys.argv[0],commandline)
+        sys.exit(1)
+
+# set ourselves up to parse command-line options
+
+from optparse import OptionParser
+
+# note that correct management of the Id string requires issuing the command
+# svn propset svn:keywords Id FILENAME
+
+parser = OptionParser(usage="usage: %prog [options] OUTPUT.xml",
+                      version="$Id: $")
+
+parser.add_option("-n", "--seedNoise",
+                  type="int", dest="seednoise", default=None,
+                  help="noise-specific seed (int) [required]")
+
+parser.add_option("-d", "--lisasimDir",
+                  type="string", dest="lisasimdir", default=None,
+                  help="location of the LISA Simulator executable for the correct signal length [will try to guess]")
+
+parser.add_option("-T", "--duration",
+                  type="float", dest="duration", default=62914560,
+                  help="total time for TDI observables (s), will be overridden by LISA Simulator installation if given [default 62914560 = 2^22 * 15]")
+
+(options, args) = parser.parse_args()
+
+# try to find the LISA Simulator...
+
+if options.seednoise == None:
+    parser.error("You must give the noise seed!")
+
+seednoise = options.seednoise
+
+if options.lisasimdir == None:
+    try:
+        import lisasimulator
+    except:
+        parser.error("You must provide the location of the LISA Simulator!")
+    
+    if options.duration == 31457280:
+        lisasimdir = lisasimulator.lisasim1yr
+    elif options.duration == 62914560:
+        lisasimdir = lisasimulator.lisasim2yr
+    else:
+        parser.error("I can only run the LISA Simulator for one or two years (2^21 or 2^22 s)!")
+    
+    if not os.path.isdir(lisasimdir):
+        parser.error("You must provide the location of the LISA Simulator!")
+
+lisasimdir = os.path.abspath(options.lisasimdir)
+
+if len(args) != 1:
+    parser.error("Beyond the options, you must specify only the output file!")
+
+outputfile = args[0]
+
+# --- make a virtual LISAsim environment
+
+here = os.getcwd()
+
+workdir = os.path.abspath(tempfile.mkdtemp(dir='.'))
+os.chdir(workdir)
+
+execfiles = ['GWconverter','InstrumentNoise','Noise_Maker','Package',
+             'Vertex1Noise','Vertex1Signals','Vertex1Times',
+             'Vertex2Noise','Vertex2Signals','Vertex2Times',
+			 'Vertex3Noise','Vertex3Signals','Vertex3Times']
+
+for f in execfiles:
+    run('ln -s %s/%s ./%s' % (lisasimdir,f,f),quiet=True)
+
+run('mkdir Binary',quiet=True)
+run('mkdir Data',quiet=True)
+run('mkdir XML',quiet=True)
+
+binfiles = ( glob.glob('%s/Binary/Vert[1-3]_*.dat' % lisasimdir) +
+             glob.glob('%s/Binary/UnVc[XYZ]*.dat' % lisasimdir) +
+             ['%s/Binary/Setup.dat' % lisasimdir] )
+
+for b in binfiles:
+    run('ln -s %s Binary/%s' % (b,os.path.basename(b)),quiet=True)
+
+os.chdir(here)
+tdifile = os.path.abspath(outputfile)
+
+# make noise!
+
+os.chdir(workdir)
+
+run('./Noise_Maker %(seednoise)s')
+
+run('rm -f sources.txt; touch sources.txt',quiet=True)
+run('./Package noise-only sources.txt %(seednoise)s')
+outxml = os.path.abspath('XML/noise-only.xml')
+
+# remove temporary files
+run('rm -f Binary/AccNoise*.dat Binary/ShotNoise*.dat Binary/XNoise*.bin Binary/YNoise*.bin Binary/ZNoise*.bin',quiet=True)
+run('rm -f Binary/M1Noise*.bin Binary/M2Noise*.bin Binary/M3Noise*.bin',quiet=True)
+
+noisefileobj = lisaxml.lisaXML(tdifile)
+noisefileobj.close()
+run('%s/bin/mergeXML.py %s %s %s' % (here,tdifile,here + '/Template/StandardLISA.xml',outxml),quiet=True)
+
+run('rm -f XML/noise-only.xml',quiet=True)
+run('rm -f XML/noise-only.bin',quiet=True)
+run('rm -f Data/*.txt',quiet=True)
+
+os.chdir(here)
+
+# ...remove the tmp dir
+
+run('rm -rf %s' % workdir,quiet=True)
+
+sys.exit(0)
---
+++ MLDCpipelines2/bin/makeTDIsignal-lisasim.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+
+__version__='$Id: $'
+
+import sys
+import glob
+import os
+import tempfile
+import re
+from distutils.dep_util import newer
+
+import lisaxml
+
+def run(command,quiet = False):
+    commandline = command % globals()
+    print "----> %s" % commandline
+    
+    try:
+        if not quiet:
+            assert(os.system(commandline) == 0)
+        else:
+            assert(os.system(commandline + ' > /dev/null') == 0)
+    except:
+        print 'Script %s failed at command "%s".' % (sys.argv[0],commandline)
+        sys.exit(1)
+
+# set ourselves up to parse command-line options
+
+from optparse import OptionParser
+
+# note that correct management of the Id string requires issuing the command
+# svn propset svn:keywords Id FILENAME
+
+parser = OptionParser(usage="usage: %prog [options] BARYCENTRIC.xml OUTPUT.xml",
+                      version="$Id: $")
+
+parser.add_option("-d", "--lisasimDir",
+                  type="string", dest="lisasimdir", default=None,
+                  help="location of the LISA Simulator executable for the correct signal length [will try to guess]")
+
+parser.add_option("-T", "--duration",
+                  type="float", dest="duration", default=62914560,
+                  help="total time for TDI observables (s), will be overridden by LISA Simulator installation if given [default 62914560 = 2^22 * 15]")
+
+(options, args) = parser.parse_args()
+
+# try to find the LISA Simulator...
+
+if options.lisasimdir == None:
+    try:
+        import lisasimulator
+    except:
+        parser.error("You must provide the location of the LISA Simulator!")
+    
+    if options.duration == 31457280:
+        lisasimdir = lisasimulator.lisasim1yr
+    elif options.duration == 62914560:
+        lisasimdir = lisasimulator.lisasim2yr
+    else:
+        parser.error("I can only run the LISA Simulator for one or two years (2^21 or 2^22 s)!")
+    
+    if not os.path.isdir(lisasimdir):
+        parser.error("You must provide the location of the LISA Simulator!")
+
+lisasimdir = os.path.abspath(options.lisasimdir)
+
+if len(args) != 2:
+    parser.error("You must specify an input file and an output file!")
+
+(inputfile,outputfile) = args
+
+if (inputfile[-4:] != '.xml') or (outputfile[-4:] != '.xml'):
+    parser.error("Sorry to be pedantic, but the files should end in .xml!")
+
+# --- make a virtual LISAsim environment
+
+here = os.getcwd()
+
+workdir = os.path.abspath(tempfile.mkdtemp(dir='.'))
+os.chdir(workdir)
+
+execfiles = ['GWconverter','InstrumentNoise','Noise_Maker','Package',
+             'Vertex1Noise','Vertex1Signals','Vertex1Times',
+             'Vertex2Noise','Vertex2Signals','Vertex2Times',
+			 'Vertex3Noise','Vertex3Signals','Vertex3Times']
+
+for f in execfiles:
+    run('ln -s %s/%s ./%s' % (lisasimdir,f,f),quiet=True)
+
+run('mkdir Binary',quiet=True)
+run('mkdir Data',quiet=True)
+run('mkdir XML',quiet=True)
+
+binfiles = ( glob.glob('%s/Binary/Vert[1-3]_*.dat' % lisasimdir) +
+             glob.glob('%s/Binary/UnVc[XYZ]*.dat' % lisasimdir) +
+             ['%s/Binary/Setup.dat' % lisasimdir] )
+
+for b in binfiles:
+    run('ln -s %s Binary/%s' % (b,os.path.basename(b)),quiet=True)
+
+os.chdir(here)
+
+# run LISA simulator to create source
+
+xmlfile = os.path.abspath(inputfile)
+binfile = re.sub('.xml','-0.bin',xmlfile)
+
+# xmlname = re.sub('\-barycentric.xml','',os.path.basename(inputfile))
+tdifile = os.path.abspath(outputfile)
+
+if '-barycentric.xml' in xmlfile:
+    xmlname = re.sub('\-barycentric.xml','',os.path.basename(xmlfile))
+else:
+    xmlname = re.sub('.xml','',os.path.basename(xmlfile))
+
+xmldest = workdir + '/XML/' + xmlname + '_Barycenter.xml'
+bindest = workdir + '/XML/' + os.path.basename(binfile)
+
+run('cp %s %s' % (xmlfile,xmldest),quiet=True)
+run('cp %s %s' % (binfile,bindest),quiet=True)
+
+os.chdir(workdir)
+
+# run LISA simulator...
+
+run('./GWconverter %s' % xmlname)
+run('./Vertex1Signals')
+run('./Vertex2Signals')
+run('./Vertex3Signals')
+
+run('echo "%s" > sources.txt' % xmlname)
+run('./Package %s sources.txt 0' % xmlname)
+
+outxml = 'XML/' + xmlname + '.xml'
+outbin = 'XML/' + xmlname + '.bin'
+
+# create an empty XML file...
+sourcefileobj = lisaxml.lisaXML(tdifile)
+sourcefileobj.close()
+
+# then add the TDI data from the LISA Simulator output
+# (this will need changing when running challenge2.py in a workdir)
+run('%s/bin/mergeXML.py %s %s %s' % (here,tdifile,here + '/Template/StandardLISA.xml',outxml),quiet=True)
+
+# then add the modified Source data included in the Barycentric file
+# (this will need changing when running challenge2.py in a workdir)
+run('%s/bin/mergeXML.py -k %s %s' % (here,tdifile,xmlfile),quiet=True)
+
+os.chdir(here)
+
+# ...remove the tmp dir
+
+run('rm -rf %s' % workdir,quiet=True)
+
+sys.exit(0)
