f18cdc0b60a96dad11623a3a2fb0aac3a927b616
Michele Vallisneri <vallis@vallis.org>
2007-07-26 16:19:50 +0000

    Removed legacy version
    git-svn-id: https://lisatools.googlecode.com/svn@479 c0d0b04f-df38-0410-a060-df9e05943d0e

--- lisaXML/io-python/setup.py
+++ lisaXML/io-python/setup.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
+from distutils.sysconfig import get_python_lib
+
+import sys
+
+# get the install path so we know where to put test.xml
+
+install_prefix = ''
+
+for arg in sys.argv:
+    if arg.startswith('--prefix='):
+        install_prefix = arg.split('=', 1)[1]
+
+install_path = get_python_lib(prefix=install_prefix)
 
 # now run the setup
 
@@ -10,5 +23,6 @@ setup(name = 'lisaxml',
       author = 'M. Vallisneri',
       author_email = 'vallis@vallis.org',
       url = 'http://svn.sourceforge.net/viewvc/lisatools',
-      packages = ['lisaxml']
+      packages = ['lisaxml'],
+      data_files = [(install_path + '/lisaxml', ['test.xml'])]
       )
