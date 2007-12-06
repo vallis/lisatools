24aef0735c816b41cbb5efc444e2144088bb1d7f
Michele Vallisneri <vallis@vallis.org>
2007-12-06 01:50:47 +0000

    Now with new synthlisa filters.
    git-svn-id: https://lisatools.googlecode.com/svn@612 c0d0b04f-df38-0410-a060-df9e05943d0e

--- MLDCwaveforms/Stochastic/Stochastic.py
+++ MLDCwaveforms/Stochastic/Stochastic.py
@@ -2,12 +2,28 @@
 
 # wavearray = []; wavearray.append(NoiseWave(noisehp,noisehc,elat,elon,0)); WaveArray(wavearray)
 
-import synthlisa
-
-import lisaxml
 import numpy
 import math
 import random
+
+import synthlisa
+
+# define BandIntFilter workaround for versions of synthlisa that do not have it
+
+if not hasattr(synthlisa,'BandIntFilter'):
+    def BandIntFilter(deltat,flow,fhi):
+        r0 = math.pi * flow * deltat
+        r1 = math.pi * fhi  * deltat
+
+        alpha0 =  (1.0 + r1) / (1.0 + r0)
+        alpha1 = -(1.0 - r1) / (1.0 + r0)
+        beta1  =  (1.0 - r0) / (1.0 + r0)
+        
+        return synthlisa.IIRFilter([alpha0,alpha1],[0.0,beta1])
+    
+    synthlisa.BandIntFilter = BandIntFilter
+
+import lisaxml
 
 lisaxml.SourceClassModules['Stochastic'] = 'Stochastic'
 
@@ -22,80 +38,75 @@ class Stochastic(lisaxml.Source):
                    ('EclipticLongitude',    'Radian',      None,    'standard ecliptic longitude'),
                    ('Polarization',         'Radian',      0,       'standard source polarization'),
                    ('PowerSpectralDensity', '(f/Hz)^n/Hz', None,    'PSD normalization at 1 Hz'),
-                   ('SpectralType',         'String',      'WhiteOmega', 'frequency dependence of PSD'),
+                   ('SpectralSlope',        '1',           -3,      'slope of PSD'),
+                   ('Flow',                 'Hertz',       1e-5,    'minimum noise frequency'),
+                   ('Fknee',                'Hertz',       1e-2,    'maximum noise frequency'),
                    ('PseudoRandomSeed',     '1',           0,       'seed for pseudorandom generators'),
                    ('InterpolationOrder',   '1',           1,       'interpolation order for pseudorandom noise') )
     
                    # some kind of random seed, too... some kind of sampling frequency...
     
+    dopolarization = True
+    
     def __init__(self,name=''):
         super(Stochastic, self).__init__('Stochastic',name)
     
     def synthesize(self,samples,deltat,inittime):
-        # set buffer length compatibly with TDI...
-        bufferlen = min(256,2**int(math.log(160/deltat,2)))
+        if inittime < 0:
+            prebuf = -inittime
+        else:
+            prebuf = 900.0
         
         # set seed if not given
         if not hasattr(self,'PseudoRandomSeed') or self.PseudoRandomSeed == 0:
             self.PseudoRandomSeed = random.randint(0,2**30)
         
         # set hc seed from other seed
-        random.seed(self.PseudoRandomSeed)
-        seedhc = random.randint(0,2**30)
+        seedhp = self.PseudoRandomSeed
+        random.seed(seedhp); seedhc = random.randint(0,2**30)
         
-        sourcehp = synthlisa.WhiteNoiseSource(bufferlen,self.PseudoRandomSeed,math.sqrt(self.PowerSpectralDensity)*math.sqrt(0.5/deltat))
-        sourcehc = synthlisa.WhiteNoiseSource(bufferlen,seedhc,               math.sqrt(self.PowerSpectralDensity)*math.sqrt(0.5/deltat))
-        
-        if self.SpectralType == 'White':
-            myfilter = synthlisa.NoFilter()
-        elif self.SpectralType == 'WhiteOmega':
-            filtera = [1, 1.50000000000000, -6.76587301587302, -10.7738095238095,
-                       20.2827380952381, 34.8402777777778, -35.3437500000000,
-                       -67.0312500000000, 39.3554687500000, 85.4213169642857,
-                       -28.7820870535714, -75.9643167162698, 13.3829752604167,
-                       48.3846028645833, -3.25995551215278, -22.2980957031250,
-                       -0.178133283342634, 7.42222013927641, 0.445333208356585,
-                       -1.76197052001953, -0.166408326890734, 0.290992101033529,
-                       0.0333706537882487, -0.0320993907867916, -0.00393296991075788,
-                       0.00221676485879081, 0.000263114770253499, -0.0000861287117004395,
-                       -8.86619091033936e-6, 1.54806507958306e-6, 
-                       1.14597025371733e-7, -8.16015970139276e-9,
-                       -2.40222092658754e-10]
-            
-            filterb = [0, -1.50000000000000, -6.76587301587302, 10.7738095238095,
-                       20.2827380952381, -34.8402777777778, -35.3437500000000,
-                       67.0312500000000, 39.3554687500000, -85.4213169642857,
-                       -28.7820870535714, 75.9643167162698, 13.3829752604167,
-                       -48.3846028645833, -3.25995551215278, 22.2980957031250,
-                       -0.178133283342634, -7.42222013927641, 0.445333208356585,
-                       1.76197052001953, -0.166408326890734, -0.290992101033529,
-                       0.0333706537882487, 0.0320993907867916, -0.00393296991075788,
-                       -0.00221676485879081, 0.000263114770253499, 0.0000861287117004395,
-                       -8.86619091033936e-6, -1.54806507958306e-6,
-                       1.14597025371733e-7, 8.16015970139276e-9,
-                       -2.40222092658754e-10]
-            
-            filtera = [1, 3.0/2, 5.0/12]
-            filterb = [0, 3.0/2, -5.0/12]
+        noisehp = self.makealphanoise(deltat,prebuf,self.PowerSpectralDensity,-self.SpectralSlope,
+                                      self.Flow,self.Fknee,self.InterpolationOrder,seedhp)
+        noisehc = self.makealphanoise(deltat,prebuf,self.PowerSpectralDensity,-self.SpectralSlope,
+                                      self.Flow,self.Fknee,self.InterpolationOrder,seedhc)
                 
-            filterb = [0.9999 * x for x in filterb]
-            
-            myfilter = synthlisa.IIRFilter(filtera,filterb)
-        else:
-            raise NotImplementedError, "Unknown SpectralType %s" % self.SpectralType
-        
         interp = synthlisa.getInterpolator(self.InterpolationOrder)
         
-        # synthlisa has long-standing problems with prebuffering...
-        noisehp = synthlisa.InterpolatedSignal(synthlisa.SignalFilter(256,sourcehp,myfilter),interp,deltat,-inittime)
-        noisehc = synthlisa.InterpolatedSignal(synthlisa.SignalFilter(256,sourcehc,myfilter),interp,deltat,-inittime)
-        
-        return synthlisa.NoiseWave(noisehp,noisehc,self.EclipticLatitude,self.EclipticLongitude,self.Polarization)
+        return synthlisa.NoiseWave(noisehp,noisehc,self.EclipticLatitude,self.EclipticLongitude,-self.Polarization)
     
     def waveforms(self,samples,deltat,inittime):
         noise = self.synthesize(samples,deltat,inittime) 
         
-        [hp,hc] = numpy.transpose(synthlisa.getobs(samples,deltat,[noise.hp,noise.hc],inittime))
+        [hp,hc] = numpy.transpose(synthlisa.getobsc(samples,deltat,[noise.hp,noise.hc],inittime))
         
         return (hp,hc)
     
+    def makealphanoise(self,deltat,prebuf,psd,alpha,flow,fhi,interplen=1,seed=0,order=None):
+        nyquistf = 0.5/deltat
+        norm = math.sqrt(psd) * math.sqrt(nyquistf) / fhi**(alpha/2.0)
+        sigbuffer = min(256,2**int(math.log(160/deltat,2)))
+                
+        wlow = 2 * math.pi * flow
+        whi  = 2 * math.pi * fhi
+        
+        if order == None:
+            order = int(1.5 * (math.log10(whi) - math.log10(wlow)) + 1)
+        
+        deltap = (math.log10(whi) - math.log10(wlow)) / order
+        
+        logp0 = math.log10(wlow) + 0.5 * (1.0 - 0.5 * alpha) * deltap
+        
+        logp = [logp0 + i * deltap for i in range(0,order)]
+        logz = [logpi + 0.5 * alpha * deltap for logpi in logp]
+        
+        thenoise = synthlisa.WhiteNoiseSource(sigbuffer,0)
+        
+        for i in range(0,order):
+            newbandfilter = synthlisa.BandIntFilter(deltat,10**logp[i]/(2 * math.pi),10**logz[i]/(2 * math.pi))
+            thenoise = synthlisa.SignalFilter(sigbuffer,thenoise,newbandfilter)
+        
+        interp = synthlisa.getInterpolator(interplen)
+        
+        return synthlisa.InterpolatedSignal(thenoise,interp,deltat,prebuf,norm)
+    
+
