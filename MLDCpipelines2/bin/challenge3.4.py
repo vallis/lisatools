02de0d6fc0c7749e72c482fb719d0781469fcd8a
Michele Vallisneri <vallis@vallis.org>
2007-12-08 01:44:37 +0000

    Better logic for finding executable.
    git-svn-id: https://lisatools.googlecode.com/svn@651 c0d0b04f-df38-0410-a060-df9e05943d0e

--- MLDCpipelines2/bin/challenge3.4.py
+++ MLDCpipelines2/bin/challenge3.4.py
@@ -16,18 +16,24 @@ def run(command):
         print 'Script %s failed at command "%s".' % (sys.argv[0],commandline)
         sys.exit(1)
 
+mydir = os.path.dirname(os.path.abspath(sys.argv[0]))
+
 # only one argument, and it's the seed...
 seed = int(sys.argv[1])
 
 random.seed(seed)
 
-# Challenge 3.4: Poisson-distributed, 6 events
+# NOTE THAT THIS SCRIPT MUST BE RUN WITH challenge.py options
+#   --synthlisa --rawMeasurements --randomizeNoise=0.2 --laserNoise=10
+
+# Challenge 3.4: Poisson-distributed, 5 events
 #                define month as 2**21 samples, 1 Hz (really 24 days)
 
 srcnum = numpy.random.poisson(5)
+srcnum = 2
 
 for srcind in range(srcnum):
     seed = seed + 1
     requestsn = random.randint(10,100)
 
-    run('bin/makesource-CosmicStringCusp.py --seed=%(seed)s -t 1048576 -r 1048576 -S %(requestsn)s -n "Cosmic String %(srcind)s" Source/Burst-3.4.%(srcind)s' % globals())
+    run('%(mydir)s/makesource-CosmicStringCusp.py --seed=%(seed)s -t 1048576 -r 1048576 -S %(requestsn)s -n "Cosmic String %(srcind)s" Source/Burst-3.4.%(srcind)s' % globals())
