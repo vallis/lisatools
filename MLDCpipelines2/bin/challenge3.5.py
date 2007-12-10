d60fca6f0db7e945ed0a8abde47b5df7cfc6c796
Michele Vallisneri <vallis@vallis.org>
2007-12-10 22:18:56 +0000

    Add LISACode key generation
    git-svn-id: https://lisatools.googlecode.com/svn@665 c0d0b04f-df38-0410-a060-df9e05943d0e

--- MLDCpipelines2/bin/challenge3.5.py
+++ MLDCpipelines2/bin/challenge3.5.py
@@ -27,3 +27,9 @@ random.seed(seed)
 # to be run for one month, at one second sampling...
 
 run('%(mydir)s/makesource-StochasticBackground.py --seed=%(seed)s --PSD=1.0e-48 --randomizePSD=0.3 --pixelRefinement=2 --nProc=%(nproc)s Immediate/Background.xml' % globals())
+
+# for LISACode
+
+# run('%(mydir)s/makesource-StochasticBackground.py --seed=%(seed)s --PSD=1.0e-48 --randomizePSD=0.3 --pixelRefinement=2 LISACode/Background.xml' % globals())
+# run('%(mydir)s/makeTDInoise-synthlisa2.py --keyOnly --seed=%(seed)s --randomizeNoise=0.2 --laserNoise=10 LISACode/Background-noise.xml' % globals())
+# run('%(mydir)s/mergeXML.py LISACode/Background-noise.xml LISACode/Background.xml' % globals())
