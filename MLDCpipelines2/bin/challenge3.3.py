c09349dcaa2a06a0f1edb11bfe49bef7aebdb2a0
Michele Vallisneri <vallis@vallis.org>
2007-12-15 04:27:38 +0000

    Use a different seed for every EMRI!
    git-svn-id: https://lisatools.googlecode.com/svn@696 c0d0b04f-df38-0410-a060-df9e05943d0e

--- MLDCpipelines2/bin/challenge3.3.py
+++ MLDCpipelines2/bin/challenge3.3.py
@@ -24,16 +24,16 @@ seed = int(sys.argv[1])
 random.seed(seed)
 
 requestsn = random.randint(10,50)
-run('%s/makesource-EMRI.py -f --seed=%s --distance=1e9 --requestSN=%s --massSMBH=1.e7 --sourceName="EMRI-3.3.1" Source/EMRI-3.3.1.xml' % (mydir,seed,requestsn))
+run('%s/makesource-EMRI.py -f --seed=%s --distance=1e9 --requestSN=%s --massSMBH=1.e7 --sourceName="EMRI-3.3.1" Source/EMRI-3.3.1.xml' % (mydir,seed+1,requestsn))
 
 requestsn = random.randint(10,50)
-run('%s/makesource-EMRI.py -f --seed=%s --distance=1e9 --requestSN=%s --massSMBH=5.e6 --sourceName="EMRI-3.3.2" Source/EMRI-3.3.2.xml' % (mydir,seed,requestsn))
+run('%s/makesource-EMRI.py -f --seed=%s --distance=1e9 --requestSN=%s --massSMBH=5.e6 --sourceName="EMRI-3.3.2" Source/EMRI-3.3.2.xml' % (mydir,seed+2,requestsn))
 
 requestsn = random.randint(10,50)
-run('%s/makesource-EMRI.py -f --seed=%s --distance=1e9 --requestSN=%s --massSMBH=5.e6 --sourceName="EMRI-3.3.3" Source/EMRI-3.3.3.xml' % (mydir,seed,requestsn))
+run('%s/makesource-EMRI.py -f --seed=%s --distance=1e9 --requestSN=%s --massSMBH=5.e6 --sourceName="EMRI-3.3.3" Source/EMRI-3.3.3.xml' % (mydir,seed+3,requestsn))
 
 requestsn = random.randint(10,50)
-run('%s/makesource-EMRI.py -f --seed=%s --distance=1e9 --requestSN=%s --massSMBH=1.e6 --sourceName="EMRI-3.3.4" Source/EMRI-3.3.4.xml' % (mydir,seed,requestsn))
+run('%s/makesource-EMRI.py -f --seed=%s --distance=1e9 --requestSN=%s --massSMBH=1.e6 --sourceName="EMRI-3.3.4" Source/EMRI-3.3.4.xml' % (mydir,seed+4,requestsn))
 
 requestsn = random.randint(10,50)
-run('%s/makesource-EMRI.py -f --seed=%s --distance=1e9 --requestSN=%s --massSMBH=1.e6 --sourceName="EMRI-3.3.5" Source/EMRI-3.3.5.xml' % (mydir,seed,requestsn))
+run('%s/makesource-EMRI.py -f --seed=%s --distance=1e9 --requestSN=%s --massSMBH=1.e6 --sourceName="EMRI-3.3.5" Source/EMRI-3.3.5.xml' % (mydir,seed+5,requestsn))
