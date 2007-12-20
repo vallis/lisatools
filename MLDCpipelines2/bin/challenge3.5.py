f12725406440f1b7ad27639bc39a6e03f7aec475
Michele Vallisneri <vallis@vallis.org>
2007-12-20 20:15:08 +0000

    Add LISACode input file generation
    git-svn-id: https://lisatools.googlecode.com/svn@710 c0d0b04f-df38-0410-a060-df9e05943d0e

--- MLDCpipelines2/bin/challenge3.5.py
+++ MLDCpipelines2/bin/challenge3.5.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import random
+import re
 
 import numpy
 
@@ -16,21 +17,46 @@ def run(command):
         print 'Script %s failed at command "%s".' % (sys.argv[0],commandline)
         sys.exit(1)
 
+def makefromtemplate(output,template,**kwargs):
+    fi = open(template,'r')
+    fo = open(output,'w')
+        
+    for line in fi:
+        repline = line
+        
+        for kw in kwargs:
+            repline = re.sub('{' + kw + '}',str(kwargs[kw]),repline)
+        
+        print >> fo, repline,
+
+
 mydir = os.path.dirname(os.path.abspath(sys.argv[0]))
 
-# two arguments, the seed...
+# two arguments, the seed and the number of processors
+# (the second argument is an "istraining" flag)
+
 seed = int(sys.argv[1])
+istraining = sys.argv[2]
 nproc = int(sys.argv[3])
 
 random.seed(seed)
 
 # to be run for one month, at one second sampling...
 
-run('%(mydir)s/makesource-StochasticBackground.py --seed=%(seed)s --PSD=1.0e-48 --randomizePSD=0.3 --pixelRefinement=2 --nProc=%(nproc)s Immediate/Background.xml' % globals())
+# run('%(mydir)s/makesource-StochasticBackground.py --seed=%(seed)s --PSD=1.0e-48 --randomizePSD=0.3 --pixelRefinement=2 --nProc=%(nproc)s Immediate/Background.xml' % globals())
 
 # for LISACode
 
-# run('%(mydir)s/makesource-StochasticBackground.py --seed=%(seed)s --PSD=1.0e-48 --randomizePSD=0.3 --pixelRefinement=2 LISACode/Background.xml' % globals())
-# run('%(mydir)s/makeTDInoise-synthlisa2.py --keyOnly --seed=%(seed)s --randomizeNoise=0.2 --laserNoise=10 LISACode/Background-noise.xml' % globals())
-# run('%(mydir)s/mergeXML.py LISACode/Background-noise.xml LISACode/Background.xml' % globals())
-# run('mv LISACode/Background-noise.xml LISACode/Background.xml')
+if len(sys.argv) > 4 and sys.argv[4] == '--dolisacode':
+    lisacodeseed = random.randint(1,10000000)
+
+    run('%(mydir)s/makesource-StochasticBackground.py --seed=%(seed)s --PSD=1.0e-48 --randomizePSD=0.3 --pixelRefinement=2 LISACode/Background-sources.xml' % globals())
+    run('%(mydir)s/makeTDInoise-synthlisa2.py --keyOnly --seed=%(seed)s --randomizeNoise=0.2 --laserNoise=10 LISACode/Background-noise.xml' % globals())
+
+    if istraining == 'True':
+        makefromtemplate('LISACode/Background.xml','Template/LISACode.xml',challengename='challenge3.5-training',randomseed=lisacodeseed)
+    else:
+        makefromtemplate('LISACode/Background.xml','Template/LISACode.xml',challengename='challenge3.5',randomseed=lisacodeseed)
+
+    run('%(mydir)s/mergeXML.py LISACode/Background.xml LISACode/Background-noise.xml LISACode/Background-sources.xml' % globals())
+    run('rm LISACode/Background-noise.xml LISACode/Background-sources.xml')
