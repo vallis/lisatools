20117da96acc60539159beffe0af36bbead3d204
Neil Cornish <neilcornish@gmail.com>
2008-03-01 22:33:29 +0000

    default values
    git-svn-id: https://lisatools.googlecode.com/svn@795 c0d0b04f-df38-0410-a060-df9e05943d0e

---
+++ MLDCwaveforms/Galaxy_General/Detector.h
@@ -0,0 +1,31 @@
+         /* ----------------  DETECTOR CONSTANTS  ---------------- */               
+
+ /* Observation time (seconds) */ 
+#define T 62914560.000000000000
+
+ /* Number of data points */ 
+#define NFFT 4194304
+
+#define dt 15.000000
+
+ /* Initial azimuthal position of the guiding center */ 
+#define kappa 0.000000
+
+ /* Initial orientation of the LISA constellation */ 
+#define lambda 0.000000
+
+ /* Mean arm length of the LISA detector (meters) */ 
+#define L 5.000000e+09
+
+ /* Photon shot noise power */ 
+#define Sps 4.000000e-22
+
+ /* Acceleration noise power */ 
+#define Sacc 9.000000e-30
+
+ /* Transfer frequency */ 
+#define fstar 0.0095426903
+
+ /* LISA orbital eccentricity */ 
+#define ec 0.0096483704
+
