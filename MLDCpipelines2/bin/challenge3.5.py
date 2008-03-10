516f1518d5e845e170dd413dff83c01690ad7417
Michele Vallisneri <vallis@vallis.org>
2008-03-10 06:09:50 +0000

    Refactor.
    git-svn-id: https://lisatools.googlecode.com/svn@814 c0d0b04f-df38-0410-a060-df9e05943d0e

--- MLDCpipelines2/bin/challenge3.5.py
+++ MLDCpipelines2/bin/challenge3.5.py
@@ -29,13 +29,15 @@ nproc = int(sys.argv[3])
 
 random.seed(seed)
 
+pixelref = 2
+
 # to be run for one month, at one second sampling...
 
-run('%(mydir)s/makesource-StochasticBackground.py --seed=%(seed)s --PSD=1.0e-47 --randomizePSD=0.3 --pixelRefinement=2 --nProc=%(nproc)s Immediate/Background.xml' % globals())
+run('%(mydir)s/makesource-StochasticBackground.py --seed=%(seed)s --PSD=1.0e-47 --randomizePSD=0.3 --pixelRefinement=%(pixelref)s --nProc=%(nproc)s Immediate/Background.xml' % globals())
 
 # now for LISACode
 
 # use the same seed for training sets so we're sure we've got the same sources
 lcseed = istraining and seed or random.randint(1,10000000)
 
-run('%(mydir)s/makesource-StochasticBackground.py --seed=%(lcseed)s --PSD=1.0e-47 --randomizePSD=0.3 --pixelRefinement=2 LISACode/source-Background.xml' % globals())
+run('%(mydir)s/makesource-StochasticBackground.py --seed=%(lcseed)s --PSD=1.0e-47 --randomizePSD=0.3 --pixelRefinement=%(pixelref)s LISACode/source-Background.xml' % globals())
