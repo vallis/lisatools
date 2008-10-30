244426eeb9e7da6d23d066d5a681ac8f90afbcfd
Stas Babak <stasbabak@googlemail.com>
2008-10-30 19:50:20 +0000

    first atempt to correct to extend to gcc 4.3
    git-svn-id: https://lisatools.googlecode.com/svn@879 c0d0b04f-df38-0410-a060-df9e05943d0e

--- MLDCwaveforms/BBH/BBHChallenge1.hh
+++ MLDCwaveforms/BBH/BBHChallenge1.hh
@@ -26,6 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <iostream>
 #include <fstream>
 #include <math.h>
+#include <cstdlib>
 #include "Constants.hh"
 #include "Macros.hh"
 #include <vector>
