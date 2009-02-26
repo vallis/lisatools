b44cb2be1f6649608226a3b3d99f4d1028d57d65
Michele Vallisneri <vallis@vallis.org>
2009-02-26 17:16:08 +0000

    Add Leopard Python 2.5.1 hack
    git-svn-id: https://lisatools.googlecode.com/svn@899 c0d0b04f-df38-0410-a060-df9e05943d0e

--- lisaXML/io-python/setup.py
+++ lisaXML/io-python/setup.py
@@ -7,13 +7,17 @@ import sys
 
 # get the install path so we know where to put test.xml
 
-install_prefix = ''
+install_prefix = None
 
 for arg in sys.argv:
     if arg.startswith('--prefix='):
         install_prefix = arg.split('=', 1)[1]
 
-install_path = get_python_lib(prefix=install_prefix)
+if install_prefix and sys.platform == 'darwin':
+    # this hack needed because of a bug in OS X Leopard's stock Python 2.5.1
+    install_path = get_python_lib(standard_lib=True,prefix=install_prefix) + '/site-packages'
+else:
+    install_path = get_python_lib(prefix=install_prefix)
 
 # now run the setup
 
