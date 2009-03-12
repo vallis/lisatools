8d7466ddb13bb1675a7dc36c6af8d1579b6d582b
Michele Vallisneri <vallis@vallis.org>
2009-03-12 23:04:26 +0000

    Allow running challenge 1B in any directory
    git-svn-id: https://lisatools.googlecode.com/svn@904 c0d0b04f-df38-0410-a060-df9e05943d0e

--- MLDCpipelines2/bin/challenge1.py
+++ MLDCpipelines2/bin/challenge1.py
@@ -16,6 +16,8 @@ def run(command):
         print 'Script %s failed at command "%s".' % (sys.argv[0],commandline)
         sys.exit(1)
 
+mydir = os.path.dirname(os.path.abspath(sys.argv[0]))
+
 parser = OptionParser(usage="usage: %prog [options] SEED...",
                       version="$Id: $")
 
@@ -58,46 +60,46 @@ seed134,seed134t = twoseeds()
 seed135,seed135t = twoseeds()
 
 
-run('bin/challenge.py -P %(nproc)s            --duration=31457280 --seed=%(seed111a)s  challenge1B.1.1a' % globals())
-run('bin/challenge.py -P %(nproc)s --training --duration=31457280 --seed=%(seed111at)s challenge1B.1.1a' % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s            --duration=31457280 --seed=%(seed111a)s  challenge1B.1.1a' % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s --training --duration=31457280 --seed=%(seed111at)s challenge1B.1.1a' % globals())
                                                                                                  
-run('bin/challenge.py -P %(nproc)s            --duration=31457280 --seed=%(seed111b)s  challenge1B.1.1b' % globals())
-run('bin/challenge.py -P %(nproc)s --training --duration=31457280 --seed=%(seed111bt)s challenge1B.1.1b' % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s            --duration=31457280 --seed=%(seed111b)s  challenge1B.1.1b' % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s --training --duration=31457280 --seed=%(seed111bt)s challenge1B.1.1b' % globals())
                                                                                                  
-run('bin/challenge.py -P %(nproc)s            --duration=31457280 --seed=%(seed111c)s  challenge1B.1.1c' % globals())
-run('bin/challenge.py -P %(nproc)s --training --duration=31457280 --seed=%(seed111ct)s challenge1B.1.1c' % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s            --duration=31457280 --seed=%(seed111c)s  challenge1B.1.1c' % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s --training --duration=31457280 --seed=%(seed111ct)s challenge1B.1.1c' % globals())
                                                                                                  
-run('bin/challenge.py -P %(nproc)s            --duration=31457280 --seed=%(seed112)s   challenge1B.1.2'  % globals())
-run('bin/challenge.py -P %(nproc)s --training --duration=31457280 --seed=%(seed112t)s  challenge1B.1.2'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s            --duration=31457280 --seed=%(seed112)s   challenge1B.1.2'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s --training --duration=31457280 --seed=%(seed112t)s  challenge1B.1.2'  % globals())
                                                                                                  
-run('bin/challenge.py -P %(nproc)s            --duration=31457280 --seed=%(seed113)s   challenge1B.1.3'  % globals())
-run('bin/challenge.py -P %(nproc)s --training --duration=31457280 --seed=%(seed113t)s  challenge1B.1.3'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s            --duration=31457280 --seed=%(seed113)s   challenge1B.1.3'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s --training --duration=31457280 --seed=%(seed113t)s  challenge1B.1.3'  % globals())
                                                                                                  
-run('bin/challenge.py -P %(nproc)s            --duration=31457280 --seed=%(seed114)s   challenge1B.1.4'  % globals())
-run('bin/challenge.py -P %(nproc)s --training --duration=31457280 --seed=%(seed114t)s  challenge1B.1.4'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s            --duration=31457280 --seed=%(seed114)s   challenge1B.1.4'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s --training --duration=31457280 --seed=%(seed114t)s  challenge1B.1.4'  % globals())
                                                                                                  
-run('bin/challenge.py -P %(nproc)s            --duration=31457280 --seed=%(seed115)s   challenge1B.1.5'  % globals())
-run('bin/challenge.py -P %(nproc)s --training --duration=31457280 --seed=%(seed115t)s  challenge1B.1.5'  % globals())
-                                                                                                 
-                                                                                                 
-run('bin/challenge.py -P %(nproc)s            --duration=31457280 --seed=%(seed121)s   challenge1B.2.1'  % globals())
-run('bin/challenge.py -P %(nproc)s --training --duration=31457280 --seed=%(seed121t)s  challenge1B.2.1'  % globals())
-                                                                                                 
-run('bin/challenge.py -P %(nproc)s            --duration=31457280 --seed=%(seed122)s   challenge1B.2.2'  % globals())
-run('bin/challenge.py -P %(nproc)s --training --duration=31457280 --seed=%(seed122t)s  challenge1B.2.2'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s            --duration=31457280 --seed=%(seed115)s   challenge1B.1.5'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s --training --duration=31457280 --seed=%(seed115t)s  challenge1B.1.5'  % globals())
                                                                                                  
                                                                                                  
-run('bin/challenge.py -P %(nproc)s            --duration=62914560 --seed=%(seed131)s   challenge1B.3.1'  % globals())
-run('bin/challenge.py -P %(nproc)s --training --duration=62914560 --seed=%(seed131t)s  challenge1B.3.1'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s            --duration=31457280 --seed=%(seed121)s   challenge1B.2.1'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s --training --duration=31457280 --seed=%(seed121t)s  challenge1B.2.1'  % globals())
                                                                                                  
-run('bin/challenge.py -P %(nproc)s            --duration=62914560 --seed=%(seed132)s   challenge1B.3.2'  % globals())
-run('bin/challenge.py -P %(nproc)s --training --duration=62914560 --seed=%(seed132t)s  challenge1B.3.2'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s            --duration=31457280 --seed=%(seed122)s   challenge1B.2.2'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s --training --duration=31457280 --seed=%(seed122t)s  challenge1B.2.2'  % globals())
                                                                                                  
-run('bin/challenge.py -P %(nproc)s            --duration=62914560 --seed=%(seed133)s   challenge1B.3.3'  % globals())
-run('bin/challenge.py -P %(nproc)s --training --duration=62914560 --seed=%(seed133t)s  challenge1B.3.3'  % globals())
                                                                                                  
-run('bin/challenge.py -P %(nproc)s            --duration=62914560 --seed=%(seed134)s   challenge1B.3.4'  % globals())
-run('bin/challenge.py -P %(nproc)s --training --duration=62914560 --seed=%(seed134t)s  challenge1B.3.4'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s            --duration=62914560 --seed=%(seed131)s   challenge1B.3.1'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s --training --duration=62914560 --seed=%(seed131t)s  challenge1B.3.1'  % globals())
                                                                                                  
-run('bin/challenge.py -P %(nproc)s            --duration=62914560 --seed=%(seed135)s   challenge1B.3.5'  % globals())
-run('bin/challenge.py -P %(nproc)s --training --duration=62914560 --seed=%(seed135t)s  challenge1B.3.5'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s            --duration=62914560 --seed=%(seed132)s   challenge1B.3.2'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s --training --duration=62914560 --seed=%(seed132t)s  challenge1B.3.2'  % globals())
+                                                                                                 
+run('%(mydir)s/challenge.py -P %(nproc)s            --duration=62914560 --seed=%(seed133)s   challenge1B.3.3'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s --training --duration=62914560 --seed=%(seed133t)s  challenge1B.3.3'  % globals())
+                                                                                                 
+run('%(mydir)s/challenge.py -P %(nproc)s            --duration=62914560 --seed=%(seed134)s   challenge1B.3.4'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s --training --duration=62914560 --seed=%(seed134t)s  challenge1B.3.4'  % globals())
+                                                                                                 
+run('%(mydir)s/challenge.py -P %(nproc)s            --duration=62914560 --seed=%(seed135)s   challenge1B.3.5'  % globals())
+run('%(mydir)s/challenge.py -P %(nproc)s --training --duration=62914560 --seed=%(seed135t)s  challenge1B.3.5'  % globals())
