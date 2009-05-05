5081983ce73456a38e979faa37744e5b1bb57a66
Stas Babak <stasbabak@googlemail.com>
2009-05-05 14:17:10 +0000

    ^&&%%$& Forgot to remove the comments
    git-svn-id: https://lisatools.googlecode.com/svn@940 c0d0b04f-df38-0410-a060-df9e05943d0e

--- MLDCevaluation/bin/evaluate-challenge3.py
+++ MLDCevaluation/bin/evaluate-challenge3.py
@@ -180,7 +180,7 @@ if (dolisasim):
 
 
 ### if key is know -- generate the data corresponding to the keys 
-"""
+
 if (options.usekey != None):
    key = options.usekey
    if (dolisasim):
@@ -188,7 +188,7 @@ if (options.usekey != None):
    if (dosynthlisa):
       run('./bin/compute-KeyData.py -k %(key)s -S  --duration=%(duration)s --timeStep=%(timestep)s %(challengename)s')
 
-"""
+
 #### IIIa. call evaluation script for synthetic LISA data
 
 if (dosynthlisa):
