9eac2a2e62dbb1b161bcaf93c1990f78b946f165
Stas Babak <stasbabak@googlemail.com>
2009-10-25 22:13:47 +0000

    added script for evaluating the errors in the parameters for EMRIs
    git-svn-id: https://lisatools.googlecode.com/svn@1086 c0d0b04f-df38-0410-a060-df9e05943d0e

---
+++ MLDCevaluation/bin/estimateEMRIerrMan.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python
+
+
+__version__='$Id:  $'
+
+
+import lisaxml
+from lisaxml.convertunit import convertUnit
+import sys, re
+import math
+import glob
+
+from math import pi, sin, cos, sqrt, fabs
+
+import synthlisa
+
+
+
+
+
+def FindDeltas(Source, ParamRange, RecSystems, KeySystem):
+    spr = "   "
+    for emri in RecSystems:
+       err = []
+       MBH = emri.MassOfSMBH
+       doit = 0
+       Source = "ExtremeMassRatioInspiral"
+#       print emri.name
+       if (re.search(Source, emri.name) != None):
+           err.append((KeySystem.EclipticLatitude - emri.EclipticLatitude)/ParamRange[0])
+           delP = (KeySystem.EclipticLongitude - emri.EclipticLongitude)/ParamRange[1]
+           delP2 = (KeySystem.EclipticLongitude + 2.*pi - emri.EclipticLongitude)/ParamRange[1]
+           delP3 = (KeySystem.EclipticLongitude -2.*pi - emri.EclipticLongitude)/ParamRange[1]
+           if (fabs(delP2 ) < fabs(delP)):
+                  delP = delP2
+           if (fabs(delP3 ) < fabs(delP)):
+                  delP = delP3
+           err.append(delP)
+           err.append((KeySystem.PolarAngleOfSpin - emri.PolarAngleOfSpin)/ParamRange[2])
+           delP = (KeySystem.AzimuthalAngleOfSpin - emri.AzimuthalAngleOfSpin)/ParamRange[3]
+           delP2 = (KeySystem.AzimuthalAngleOfSpin + 2.*pi - emri.AzimuthalAngleOfSpin)/ParamRange[3]
+           delP3 = (KeySystem.AzimuthalAngleOfSpin - 2.*pi - emri.AzimuthalAngleOfSpin)/ParamRange[3]
+           if (fabs(delP2 ) < fabs(delP)):
+               delP = delP2
+           if (fabs(delP3 ) < fabs(delP)):
+                      delP = delP3
+           err.append(delP)
+           err.append((KeySystem.Spin - emri.Spin)/ParamRange[4])
+           err.append((KeySystem.MassOfCompactObject - emri.MassOfCompactObject)/ParamRange[5])
+           err.append((KeySystem.MassOfSMBH - emri.MassOfSMBH)/ParamRange[6])
+           err.append((KeySystem.InitialAzimuthalOrbitalFrequency - emri.InitialAzimuthalOrbitalFrequency)\
+                        /KeySystem.InitialAzimuthalOrbitalFrequency)
+           delP = (KeySystem.InitialAzimuthalOrbitalPhase - emri.InitialAzimuthalOrbitalPhase)/ParamRange[8]
+           delP2 = (KeySystem.InitialAzimuthalOrbitalPhase + 2.*pi - emri.InitialAzimuthalOrbitalPhase)/ParamRange[8]
+           delP3 = (KeySystem.InitialAzimuthalOrbitalPhase - 2.*pi - emri.InitialAzimuthalOrbitalPhase)/ParamRange[8]
+           if (fabs(delP2 ) < fabs(delP)):
+                delP = delP2
+           if (fabs(delP3 ) < fabs(delP)):
+                delP = delP3
+           err.append(delP)
+           err.append((KeySystem.InitialEccentricity - emri.InitialEccentricity)/ParamRange[9])
+           delP = (KeySystem.InitialTildeGamma - emri.InitialTildeGamma)/ParamRange[10]
+           delP2 = (KeySystem.InitialTildeGamma + 2.*pi - emri.InitialTildeGamma)/ParamRange[10]
+           delP3 = (KeySystem.InitialTildeGamma - 2.*pi - emri.InitialTildeGamma)/ParamRange[10]
+           if (fabs(delP2 ) < fabs(delP)):
+                delP = delP2
+           if (fabs(delP3 ) < fabs(delP)):
+                delP = delP3
+           err.append(delP)
+           delP = (KeySystem.InitialAlphaAngle - emri.InitialAlphaAngle)/ParamRange[11]
+           delP2 = (KeySystem.InitialAlphaAngle + 2.*pi - emri.InitialAlphaAngle)/ParamRange[11]
+           delP3 = (KeySystem.InitialAlphaAngle -2*pi - emri.InitialAlphaAngle)/ParamRange[11]
+           if (fabs(delP2 ) < fabs(delP)):
+                delP = delP2
+           if (fabs(delP3 ) < fabs(delP)):
+                delP = delP3
+           err.append(delP)
+           err.append((KeySystem.LambdaAngle - emri.LambdaAngle)/ParamRange[12])
+           err.append((KeySystem.Distance - emri.Distance)/KeySystem.Distance)
+           rec = " "
+           for i in xrange(len(err)):
+               rec += str(err[i]) + spr + '&' + spr
+           print rec+"\n"
+            
+    return  err
+
+from optparse import OptionParser
+    
+parser = OptionParser(usage="usage: %prog [options] KeyFile.xml source_name ResultFile.xml source_name OutputFile", version="$Id: $")
+
+
+parser.add_option("-e", "--deltae",
+                      type="float", dest="de", default=0.15,
+                      help="range of e [default 0.15]")
+
+(options, args) = parser.parse_args()
+
+if len(args) < 5:
+    parser.error("not enough parameters, run with --help option ")
+
+keyFile = args[0]
+keyS = args[1]
+datFile = args[2]
+datS = args[3]
+outF = args[4]
+
+
+ParamRange = []
+ParamRange.append(pi) # 0-latitude
+ParamRange.append(2.*pi)  # 1-longitude
+ParamRange.append(pi)  # 2-PolarAngleOfSpin
+ParamRange.append(2.*pi)  # 3-AzimuthalAngleOfSpin
+ParamRange.append(0.2)  # 4-Spin
+ParamRange.append(1.0)  # 5-MassOfCompactObject
+ParamRange.append(1.e6)  # 6-MassOfSMBH (for 1.3.1) should be 5.e5 for 132,3 and 1.e5 for 134,5
+ParamRange.append("NA")  # 7-InitialAzimuthalOrbitalFrequency
+ParamRange.append(2.*pi)  # 8-InitialAzimuthalOrbitalPhase
+ParamRange.append(options.de)  # 9- InitialEccentricity (guess)
+ParamRange.append(2.*pi)  # 10-InitialTildeGamma
+ParamRange.append(2.*pi)  # 11-InitialAlphaAngle
+ParamRange.append(pi)  # 12-LambdaAngle
+ParamRange.append("NA")  # 13-Distance
+ParamRange.append(5.e7) # 14 to be filled up with Plunge time
+
+print ParamRange
+
+print "Dealing with ", keyFile
+inputXML = lisaxml.readXML(keyFile)
+KeySystems = inputXML.getLISASources()
+
+   
+SourceXML = lisaxml.readXML(datFile)
+RecSystems = SourceXML.getLISASources()
+
+fout = open(outF, 'w')
+
+spr = "    "
+for key in KeySystems:
+    #print "Keys: ", key.name
+    if (key.name == keyS):
+        for emri in RecSystems:
+           err = []
+	   keyPar = []
+	   #print datS,"  -   ", emri.name
+	   rec = " \n"
+	   if (emri.name == datS):
+              print "emri : ", emri.name
+              rec = rec + "EclipticLatitude:    "
+	      rec = rec + str((key.EclipticLatitude - emri.EclipticLatitude)/ParamRange[0]) + \
+                        spr + str((key.EclipticLatitude - emri.EclipticLatitude)/key.EclipticLatitude) + "\n"
+	      delP = (key.EclipticLongitude - emri.EclipticLongitude)/ParamRange[1]
+              delP2 = (key.EclipticLongitude + 2.*pi - emri.EclipticLongitude)/ParamRange[1]
+              delP3 = (key.EclipticLongitude -2.*pi - emri.EclipticLongitude)/ParamRange[1]
+              if (fabs(delP2 ) < fabs(delP)):
+                  delP = delP2
+              if (fabs(delP3 ) < fabs(delP)):
+                  delP = delP3
+	      rec = rec + "EclipticLongitude:   " 
+	      rec = rec + str(delP) + spr + str(delP*ParamRange[1]/key.EclipticLongitude) + "\n"
+              delP = (key.PolarAngleOfSpin - emri.PolarAngleOfSpin)
+	      rec = rec + "PolarAngleOfSpin:    "
+	      rec = rec + str(delP/ParamRange[2]) + spr + str(delP/key.PolarAngleOfSpin) + "\n"
+              delP = (key.AzimuthalAngleOfSpin - emri.AzimuthalAngleOfSpin)/ParamRange[3]
+              delP2 = (key.AzimuthalAngleOfSpin + 2.*pi - emri.AzimuthalAngleOfSpin)/ParamRange[3]
+              delP3 = (key.AzimuthalAngleOfSpin - 2.*pi - emri.AzimuthalAngleOfSpin)/ParamRange[3]
+              if (fabs(delP2 ) < fabs(delP)):
+                 delP = delP2
+              if (fabs(delP3 ) < fabs(delP)):
+                 delP = delP3
+	      rec = rec + "AzimuthalAngleOfSpin:   "
+	      rec = rec + str(delP) + spr + str(delP*ParamRange[3]/key.AzimuthalAngleOfSpin) + "\n"
+              delP = (key.Spin - emri.Spin)
+	      rec = rec + "Spin:     "
+	      rec = rec + str(delP/ParamRange[4]) + spr + str(delP/key.Spin) + "\n"
+              delP = key.MassOfCompactObject - emri.MassOfCompactObject
+	      rec = rec + "MassOfCompactObject:     "
+	      rec = rec + str(delP/ParamRange[5]) + spr + str(delP/key.MassOfCompactObject) + "\n"
+              delP = key.MassOfSMBH - emri.MassOfSMBH
+	      rec = rec + "MassOfSMBH:    "
+	      rec = rec + str(delP/ParamRange[6]) + spr + str(delP/key.MassOfSMBH) + "\n"
+	      rec  = rec + "InitialAzimuthalOrbitalFrequency:     "
+              rec = rec + str((key.InitialAzimuthalOrbitalFrequency - emri.InitialAzimuthalOrbitalFrequency)\
+                        /key.InitialAzimuthalOrbitalFrequency) + "\n"
+              delP = (key.InitialAzimuthalOrbitalPhase - emri.InitialAzimuthalOrbitalPhase)/ParamRange[8]
+              delP2 = (key.InitialAzimuthalOrbitalPhase + 2.*pi - emri.InitialAzimuthalOrbitalPhase)/ParamRange[8]
+              delP3 = (key.InitialAzimuthalOrbitalPhase - 2.*pi - emri.InitialAzimuthalOrbitalPhase)/ParamRange[8]
+              if (fabs(delP2 ) < fabs(delP)):
+                  delP = delP2
+              if (fabs(delP3 ) < fabs(delP)):
+                  delP = delP3
+	      rec = rec + "InitialAzimuthalOrbitalPhase:    "
+	      rec = rec + str(delP) + spr + str(delP*ParamRange[8]/key.InitialAzimuthalOrbitalPhase) + "\n"
+              delP = key.InitialEccentricity - emri.InitialEccentricity
+	      rec = rec + "InitialEccentricity:     " 
+	      rec = rec + str(delP/ParamRange[9]) + spr +  str(delP/key.InitialEccentricity) + "\n"
+              delP = (key.InitialTildeGamma - emri.InitialTildeGamma)/ParamRange[10]
+              delP2 = (key.InitialTildeGamma + 2.*pi - emri.InitialTildeGamma)/ParamRange[10]
+              delP3 = (key.InitialTildeGamma - 2.*pi - emri.InitialTildeGamma)/ParamRange[10]
+              if (fabs(delP2 ) < fabs(delP)):
+                 delP = delP2
+              if (fabs(delP3 ) < fabs(delP)):
+                 delP = delP3
+	      rec = rec + "InitialTildeGamma:      "
+	      rec =  rec + str(delP) +  spr + str(delP*ParamRange[10]/key.InitialTildeGamma) + "\n"
+              delP = (key.InitialAlphaAngle - emri.InitialAlphaAngle)/ParamRange[11]
+              delP2 = (key.InitialAlphaAngle + 2.*pi - emri.InitialAlphaAngle)/ParamRange[11]
+              delP3 = (key.InitialAlphaAngle -2*pi - emri.InitialAlphaAngle)/ParamRange[11]
+              if (fabs(delP2 ) < fabs(delP)):
+                  delP = delP2
+              if (fabs(delP3 ) < fabs(delP)):
+                  delP = delP3
+	      rec =  rec + "InitialAlphaAngle:    "
+	      rec = rec + str(delP) + spr + str(delP*ParamRange[11]/key.InitialAlphaAngle) + "\n"
+              delP = key.LambdaAngle - emri.LambdaAngle
+	      rec = rec + "LambdaAngle:      " 
+	      rec = rec + str(delP/ParamRange[12]) + spr + str(delP/key.LambdaAngle) + "\n"
+	      rec = rec + "Distance:    "
+	      rec = rec + str((key.Distance - emri.Distance)/key.Distance) + "\n"
+	      fout.write(rec + "\n")	
+	     
+fout.close()	      
+
+
+
+    #MBH = emri.MassOfSMBH
+    #Source = "ExtremeMassRatioInspiral"
+	    
+#	    Errors  = FindDeltas("EMRI-1", ParamRange, RecSystems, KeySystem)
+#            print Errors
+#            print ParamRange
+#            print xmlfile
+#            print xmlKey
+#            sys.exit(0)
+"""        elif (re.search('3.2', xmlKey) != None and re.search('3.2', xmlfile) != None):
+            print "processing file", xmlfile
+            ParamRange[6] = 5.e5
+            Errors  = FindDeltas("EMRI-2", ParamRange, RecSystems, KeySystem)
+        elif (re.search('3.3', xmlKey) != None and re.search('3.3', xmlfile) != None):
+            print "processing file", xmlfile
+            ParamRange[6] = 5.e5
+            Errors  = FindDeltas("EMRI-3", ParamRange, RecSystems, KeySystem)
+        elif (re.search('3.4', xmlKey) != None and re.search('3.4', xmlfile) != None):
+            print "processing file", xmlfile
+            ParamRange[6] = 1.e5
+            Errors  = FindDeltas("EMRI-4", ParamRange, RecSystems, KeySystem)
+        elif (re.search('3.5', xmlKey) != None and re.search('3.5', xmlfile) != None):
+            print "processing file", xmlfile
+            ParamRange[6] = 1.e5
+            Errors  = FindDeltas("EMRI-5", ParamRange, RecSystems, KeySystem)
+        SourceXML.close()
+#        print "Errors"
+#        print Errors
+    inputXML.close()    
+"""
