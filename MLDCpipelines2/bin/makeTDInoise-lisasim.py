91ca38636a9e7684b21eed8b1e4ee5c5f5512765
Michele Vallisneri <vallis@vallis.org>
2009-10-28 17:58:10 +0000

    Allow datasets of arbitrary offset, length, and timestep
    git-svn-id: https://lisatools.googlecode.com/svn@1104 c0d0b04f-df38-0410-a060-df9e05943d0e

--- MLDCpipelines2/bin/makeTDInoise-lisasim.py
+++ MLDCpipelines2/bin/makeTDInoise-lisasim.py
@@ -7,6 +7,7 @@ import glob
 import os
 import tempfile
 import re
+import math
 from distutils.dep_util import newer
 
 import lisaxml
@@ -23,6 +24,81 @@ def run(command,quiet = False):
     except:
         print 'Script %s failed at command "%s".' % (sys.argv[0],commandline)
         sys.exit(1)
+
+
+def copylisasim(lisasimdir,workdir):
+    if not os.path.isdir(lisasimdir):
+        print "makeTDIsignal-lisasim.py: cannot find lisasim directory %s!" % lisasimdir
+        sys.exit(1)
+    
+    here = os.getcwd()
+    os.chdir(workdir)
+    
+    execfiles = ['GWconverter','InstrumentNoise','Noise_Maker','Package',
+                 'Vertex1Noise','Vertex1Signals','Vertex1Times',
+                 'Vertex2Noise','Vertex2Signals','Vertex2Times',
+    			 'Vertex3Noise','Vertex3Signals','Vertex3Times']
+    
+    for f in execfiles:
+        run('ln -s %s/%s ./%s' % (lisasimdir,f,f),quiet=True)
+    
+    run('mkdir Binary',quiet=True)
+    run('mkdir Data',quiet=True)
+    run('mkdir XML',quiet=True)
+    
+    binfiles = ( glob.glob('%s/Binary/Vert[1-3]_*.dat' % lisasimdir) +
+                 glob.glob('%s/Binary/UnVc[XYZ]*.dat' % lisasimdir) +
+                 ['%s/Binary/Setup.dat' % lisasimdir] )
+    
+    for b in binfiles:
+        run('ln -s %s Binary/%s' % (b,os.path.basename(b)),quiet=True)
+    
+    os.chdir(here)
+
+
+def filterfile(infilename,outfilename,dictionary):
+    outfile = open(outfilename,'w')
+    
+    for line in open(infilename,'r'):
+        for key in dictionary:
+            line = re.sub('<' + key + '>',str(dictionary[key]),line)
+        
+        outfile.write(line)
+    
+    outfile.close()
+
+
+def compilelisasim(workdir,T,dt,t0=0):
+    samples = int(T / dt)
+    npow = int(math.log(samples,2))
+    
+    if 2**npow != samples:
+        print "compilelisasim(): I can only call lisasim with a power-of-two number of samples!"
+        sys.exit(1)
+    
+    lisasimtar = execdir + '/../../Packages/Simulator-2.1.1c.tar.gz'
+    
+    assert(0 == os.system('tar zxf %s -C %s' % (lisasimtar,workdir)))
+    
+    # annoying, the tar unpacks in its own directory; copy files over
+    assert(0 == os.system('cp -R %s/Simulator-2.1.1c/* %s' % (workdir,workdir)))
+    assert(0 == os.system('rm -rf %s/Simulator-2.1.1c' % workdir))
+    
+    filterfile(execdir + '/../../Packages/LISASimulator/Compile',workdir + '/Compile',{})
+    filterfile(execdir + '/../../Packages/LISASimulator/LISAconstants-generic.h',workdir + '/LISAconstants.h',
+               {'npow': npow,'NFFT': samples,'T0': t0,'T': T,'dt': dt})
+    
+    here = os.getcwd()
+    os.chdir(workdir)
+            
+    if options.gsldir != None:
+        assert(0 == os.system('./Compile --gsl=' + options.gsldir))
+    else:
+        assert(0 == os.system('./Compile'))
+    
+    assert(0 == os.system('./Setup'))
+    os.chdir(here)
+
 
 execdir = os.path.dirname(os.path.abspath(sys.argv[0]))
 
@@ -52,6 +128,10 @@ parser.add_option("-d", "--timeStep",
                   type="float", dest="timestep", default=15.0,
                   help="timestep for TDI observable sampling (s), will be overridden by LISA Simulator installation if given [default 15]")
 
+parser.add_option("-g", "--gsldir",
+                  type="string", dest="gsldir", default=None,
+                  help="location of GSL [will try to guess]")
+
 (options, args) = parser.parse_args()
 
 # try to find the LISA Simulator...
@@ -67,19 +147,18 @@ if options.lisasimdir == None:
     except:
         parser.error("You must provide the location of the LISA Simulator!")
     
-    if options.duration == 31457280:
+    if options.duration == 31457280 and options.timestep == 15.0:
         lisasimdir = lisasimulator.lisasim1yr
     elif options.duration == 62914560 and options.timestep == 15.0:
         lisasimdir = lisasimulator.lisasim2yr
-    elif options.duration == 62914560 and options.timestep == 1.875:
-        lisasimdir = lisasimulator.lisasim2yrhigh
-    else:
-        parser.error("I can only run the LISA Simulator for one year at 15 s, or two years at 15 or 1.875 s!")
-    
-    if not os.path.isdir(lisasimdir):
-        parser.error("You must provide the location of the LISA Simulator!")
 else:
     lisasimdir = os.path.abspath(options.lisasimdir)
+
+if options.gsldir == None:
+    try:
+        options.gsldir = re.match('(.*)/lib/.*',lisaxml.__file__).group(1)
+    except:
+        pass
 
 if len(args) != 1:
     parser.error("Beyond the options, you must specify only the output file!")
@@ -91,28 +170,12 @@ outputfile = args[0]
 here = os.getcwd()
 
 workdir = os.path.abspath(tempfile.mkdtemp(dir='.'))
-os.chdir(workdir)
 
-execfiles = ['GWconverter','InstrumentNoise','Noise_Maker','Package',
-             'Vertex1Noise','Vertex1Signals','Vertex1Times',
-             'Vertex2Noise','Vertex2Signals','Vertex2Times',
-			 'Vertex3Noise','Vertex3Signals','Vertex3Times']
+if (options.duration == 31457280 or options.duration == 62914560) and options.timestep == 15.0:
+    copylisasim(lisasimdir,workdir)
+else:
+    compilelisasim(workdir,options.duration,options.timestep)
 
-for f in execfiles:
-    run('ln -s %s/%s ./%s' % (lisasimdir,f,f),quiet=True)
-
-run('mkdir Binary',quiet=True)
-run('mkdir Data',quiet=True)
-run('mkdir XML',quiet=True)
-
-binfiles = ( glob.glob('%s/Binary/Vert[1-3]_*.dat' % lisasimdir) +
-             glob.glob('%s/Binary/UnVc[XYZ]*.dat' % lisasimdir) +
-             ['%s/Binary/Setup.dat' % lisasimdir] )
-
-for b in binfiles:
-    run('ln -s %s Binary/%s' % (b,os.path.basename(b)),quiet=True)
-
-os.chdir(here)
 tdifile = os.path.abspath(outputfile)
 
 # make noise!
@@ -140,7 +203,6 @@ run('rm -f Data/*.txt',quiet=True)
 os.chdir(here)
 
 # ...remove the tmp dir
-
 run('rm -rf %s' % workdir,quiet=True)
 
 sys.exit(0)
