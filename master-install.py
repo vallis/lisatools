6da5b8542d0c45bfe95b46563ebdc87762e097a5
Michele Vallisneri <vallis@vallis.org>
2012-07-11 18:22:09 +0000

    Newer synthlisa
    git-svn-id: https://lisatools.googlecode.com/svn@1205 c0d0b04f-df38-0410-a060-df9e05943d0e

--- master-install.py
+++ master-install.py
@@ -500,9 +500,9 @@ os.chdir(here)
 
 print "--> Checking synthLISA"
 
-if checkpackage('synthlisa') < LooseVersion('1.4.3') or newsynthlisa:
+if checkpackage('synthlisa') < LooseVersion('2.0.0') or newsynthlisa:
     print "--> Installing Synthetic LISA"
-    installpackage('http://www.vallis.org/software/synthLISA-1.4.3.tar.gz',prefix=libdir,keepdownload=False)
+    installpackage('http://www.vallis.org/software/synthLISA-2.0.0.tar.gz',prefix=libdir,keepdownload=False)
 
 # install/check install for LISACode
 
